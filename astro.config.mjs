a870408c1b3995a69e75185c73ecd7da42d8bc00
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-12-12 13:37:05 -0600

    chore(vercel): publish latest to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+  // Enable React to support React JSX components.
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
