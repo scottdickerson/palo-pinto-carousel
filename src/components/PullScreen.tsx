9a16b2493202d762f70f3577a9cb2be4b91188f6
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-04-05 16:11:41 -0500

    fix(sendtopullscreen): was not marked as a client side component

--- src/components/PullScreen.tsx
+++ src/components/PullScreen.tsx
@@ -4,9 +4,11 @@ export const PullScreen = () => {
             href="/en/details/HomeForMillenia"
             className="flex flex-col items-center justify-center h-screen w-screen font-calderShadow"
         >
-            <h1 className="uppercase  text-[128px]">Touch To Begin</h1>
-            <div className="text-center text-[44px] w-[800px] h-20 pt-1  gap-4 bg-title bg-no-repeat bg-cover">
-                Lorem Ipsum Dolor Dus
+            <h1 className="uppercase  text-[128px] animate-pulse">
+                Touch To Begin
+            </h1>
+            <div className="text-center text-[44px] h-20 pt-1 pl-10 pr-10 gap-4 bg-title bg-no-repeat bg-cover">
+                Palo Pinto Mountains State Park
             </div>
         </a>
     )
